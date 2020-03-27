08b9bcbe8a608e6714b7d0e622844c9506df9ef6
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2020-03-27 20:11:52 +0200

    add install script to setup symlinks to dotfiles

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+dots=~/dotfiles
+
+# tmux
+ln -sf $dots/tmux/tmux.conf ~/.tmux.conf
+#ln -sf $dots/tmux/tmuxline-tomorrow.conf ~/.tmuxline.conf
+ln -sf $dots/tmux/tmuxline-gruvbox.conf ~/.tmuxline.conf
+
+# vim
+ln -sf $dots/vim/vimrc ~/.vimrc
+ln -sf $dots/vim/ ~/.vim
