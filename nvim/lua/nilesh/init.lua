319d6036687832d96644bec2084712839139485d
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2022-12-15 13:18:23 +0200

    add: styling to floating windows

--- nvim/lua/nilesh/init.lua
+++ nvim/lua/nilesh/init.lua
@@ -158,6 +158,23 @@ cmp.setup {
   },
 }
 
+-- Give floating windows borders
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
+})
+
+vim.diagnostic.config({
+    float = {
+        severity_sort = true,
+        border = "rounded",
+    },
+    severity_sort = true,
+})
+
 local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
