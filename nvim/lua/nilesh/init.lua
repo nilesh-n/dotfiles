bb0642f4493213a1de6d4db047ea6a1e8def4126
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2023-03-24 19:32:16 +0200

    update: nvim-cmp config

--- nvim/lua/nilesh/init.lua
+++ nvim/lua/nilesh/init.lua
@@ -119,42 +119,47 @@ vim.o.completeopt = 'menuone,noselect'
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-y>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       else
         fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end,
-  },
-  sources = {
-    { name = 'nvim_lsp' },
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources(
     {
-      name = "buffer",
-      keyword_length = 3,
-      opts = {
-        get_bufnrs = function()
-          return vim.api.nvim_list_bufs()
-        end
-      }
+      { name = 'nvim_lsp' },
     },
-  },
+    {
+      {
+        name = "buffer",
+        keyword_length = 3,
+        options = {
+          get_bufnrs = function()
+            return vim.api.nvim_list_bufs()
+          end
+        }
+      },
+    }
+  )
 }
 
 -- Give floating windows borders
